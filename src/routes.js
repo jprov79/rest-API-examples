7377d816e2cee35da678f17b8d4be4bbc9d708db
Yasaman-A <yamannejad@mtroyal.ca>
2020-11-17 11:19:20 -0700

    added more endpoints

--- src/routes.js
+++ src/routes.js
@@ -6,25 +6,69 @@ module.exports.register = (app, database) => {
         res.status(200).send("You did it:) I am now running with Firestore! ").end();
     });
 
+    //Ex. http://localhost:8080/api/emp
+    //Ex. http://localhost:8080/api/emp?name=Demo%20User
+    app.get('/api/emp', async (req, res) => {
+        let ref;
+        if (req.query.name) {
+            let _name = req.query.name;
+            ref = database.collection("rest_emp").where("name", "==", _name);
+        } else {
+            ref = database.collection("rest_emp");
+        }
+        ref.get().then((collections) => {
+            let values = []
+            collections.forEach((doc) => {
+                values.push(doc.data());
+            })
+            res.status(200).send(JSON.stringify(values)).end();
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send(JSON.stringify("Invalid Request")).end()
+        });
+    });
 
-    app.get('/api/user', async (req, res) => {
-        const ref = database.collection("user").doc("test@test.com");
-        ref.get().then(x => {
-            res.status(200).send(JSON.stringify(x.data())).end();
+    //Ex. http://localhost:8080/api/user/SiSeyvV9A5bJXAupal4HjoODpzt2
+    app.get('/api/emp/:id', async (req, res) => {
+        let _id = req.params.id;
+        const ref = database.collection("rest_emp").doc(_id);
+        ref.get().then(doc => {
+            res.status(200).send(JSON.stringify(doc.data())).end();
         })
             .catch(() => res.status(500).send(JSON.stringify("Invalid Request")).end());
     });
 
-
-    app.post('/api/user', async (req, res) => {
+    //Ex. http://localhost:8080/api/user
+    // {
+    //     "name": "Posted User",
+    // }
+    app.post('/api/emp', async (req, res) => {
         let _name = req.body.name;
-        let _email = req.body.email;
+
+        const newRecord = {
+            "name": _name
+        }
+        const ref = database.collection("rest_emp");
+        ref.add(newRecord)
+            .then(() => { res.status(201).send("User added successfully!"); })
+            .catch(() => { res.status(500).send("Invalid Request!"); });
+    });
+
+    //Ex. http://localhost:8080/api/user
+    // {
+    //     "name": "Patched User",
+    //     "id": "phYAzAMz8TJHzwvpWOUf"
+    // }
+
+    app.patch('/api/emp', async (req, res) => {
+        let _name = req.body.name;
+        let _id = req.body.id;
 
         const userCredential = {
             "name": _name
         }
 
-        const ref = database.collection("user").doc(_email);
+        const ref = database.collection("rest_emp").doc(_id);
         ref.set(userCredential)
             .then(() => { res.status(201).send("User added successfully!"); })
             .catch(() => { res.status(500).send("Invalid Request!"); });
